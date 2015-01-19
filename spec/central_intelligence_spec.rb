06e74947b67ff10e3ea8b4ad71fdc88992a5a4fc
Ritchie Young <ritchiey@gmail.com>
2015-01-19 14:47:30 +0800

    generating template with return value

--- spec/central_intelligence_spec.rb
+++ spec/central_intelligence_spec.rb
@@ -1,30 +1,44 @@
 require 'pry'
 require 'rspec'
 
+class RSpec::Mocks::Proxy
+
+  alias_method :old_message_received, :message_received
+
+  def message_received(message, *args, &block)
+    old_message_received(message, *args, &block).tap do |return_value|
+      CentralIntelligence.record object, message, args, block, return_value
+    end
+  end
+
+end
+
+
 class CentralIntelligence
 
-  attr_reader :recorded_messages, :generated_specs
+  def self.recorded_messages
+    @recorded_messages
+  end
 
-  def initialize
+  def self.generated_specs
+    @generated_specs ||= generate_specs
+  end
+
+  def self.reset
     @recorded_messages = []
+    @generated_specs = nil
   end
 
-  def interrogate_spies
-    @recorded_messages  = ::RSpec::Mocks.space.proxies.map do |pair|
-      proxy = pair.last
-      class_name = proxy.object.class.name
-      proxy.instance_variable_get('@messages_received').map do |message|
-        {
-          class_name: class_name,
-          message: message[0],
-          args: message[1],
-          return_value: 42 # slime
-        }
-      end
-    end.flatten
+  def self.record(object, message, args, block, return_value)
+    @recorded_messages << {
+      class_name: object.class.name,
+      message: message,
+      args: args,
+      return_value: return_value
+    }
   end
 
-  def generate_specs
+  def self.generate_specs
     @generated_specs = <<-GENERATED
 describe Person do
 
@@ -44,21 +58,23 @@ GENERATED
 
 end
 
+
 describe CentralIntelligence do
 
   it "records the messages sent to a spy" do
-    ci = CentralIntelligence.new
+
+    CentralIntelligence.reset
+
     o = Object.new
-    allow(o).to receive(:a_message).with(:a_parameter).and_return(42)
+    expect(o).to receive(:a_message).with(:a_parameter).and_return(42)
     o.a_message(:a_parameter)
-    ci.interrogate_spies
-    expect(ci.recorded_messages).to match_array([{
+
+    expect(CentralIntelligence.recorded_messages).to match_array([{
       class_name: 'Object',
       message: :a_message,
       args: [:a_parameter],
       return_value: 42
     }])
-
   end
 
   class Person
@@ -75,34 +91,28 @@ describe CentralIntelligence do
 
 
   it "can generate required specs" do
-    pending "ability to get return value from method call"
+    CentralIntelligence.reset
+
     # run our top-level spec here
     #
     bob = Person.new('Dick', 'Jones')
     expect(bob).to receive(:full_name).and_return("Dick Jones")
     expect(bob.welcome).to eq "Welcome to OCP, I'm Dick Jones"
 
-    #
-    # this would normally be in an after(:each) block
-    ci = CentralIntelligence.new
-    ci.interrogate_spies
-
     # did it correctly record the method called
-    expect(ci.recorded_messages).to match_array(
+    expect(CentralIntelligence.recorded_messages).to match_array(
       [
         {
           class_name: 'Person',
           message: :full_name,
-          args: []
+          args: [],
+          return_value: "Dick Jones"
         }
       ]
     )
 
-    # this would normally be in an after(:all) block
-    ci.generate_specs
-
-    # did it generrate an in-memory version of the specs
-    expect(ci.generated_specs).to eq <<-GENERATED
+    # did it generate an in-memory version of the specs?
+    expect(CentralIntelligence.generated_specs).to eq <<-GENERATED
 describe Person do
 
   describe "#full_name" do
